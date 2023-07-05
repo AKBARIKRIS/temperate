e7d8f90fabfe887ec384d9fe533c161cadc0629a
AKBARIKRIS <137590850+AKBARIKRIS@users.noreply.github.com>
2023-07-05 17:54:23 +0530

    Add files via upload

---
+++ MONTH.C
@@ -0,0 +1,65 @@
+#include<stdio.h>
+#include<conio.h>
+ void main()
+ {
+  int b,c;
+  clrscr();
+
+  printf(" PREES OF NO= 1 \n");
+  printf(" PREES OF NO= 2 \n");
+  printf(" PREES OF NO= 3 \n");
+  printf(" PREES OF NO= 4 \n");
+  printf(" PREES OF NO= 5 \n");
+  printf(" PREES OF NO= 6 \n");
+  printf(" PREES OF NO= 7 \n");
+  printf(" PREES OF NO= 8 \n");
+  printf(" PREES OF NO= 9 \n");
+  printf(" PREES OF NO= 10 \n");
+  printf(" PREES OF NO= 11 \n");
+  printf(" PREES OF NO= 12 \n");
+
+
+  printf(" ENTER BIRTHDAY MONTH NO:-");
+  scanf("%d",&c);
+  switch(c)
+  {
+   case 1:
+	 printf(" YOUR BIRTHDAY MONTH IS JANUARY ",1);
+	 break;
+   case 2:
+	 printf(" YOUR BIRTHDAY MONTH IS FEBRUARUY ",2);
+	 break;
+   case 3:
+	 printf(" YOUR BIRTHDAY MONTH IS MARCH ",3);
+	 break;
+   case 4:
+	 printf(" YOUR BIRTHDAY MONTH IS APRIL ",4);
+	 break;
+   case 5:
+	 printf(" YOUR BIRTHDAY MONTH IS MAY ",5);
+	 break;
+   case 6:
+	 printf(" YOUR BIRTHDAY MONTH IS JUNE ",6);
+	 break;
+   case 7:
+	 printf(" YOUR BIRTHDAY MONTH IS JULY ",7);
+	 break;
+   case 8:
+	 printf(" YOUR BIRTHDAY MONTH IS AUGUST ",8);
+	 break;
+   case 9:
+	 printf(" YOUR BIRTHDAY MONTH IS SEPTEMBER ",9);
+	 break;
+   case 10:
+	 printf(" YOUR BIRTHDAY MONTH IS OCTOBER ",10);
+	 break;
+   case 11:
+	 printf(" YOUR BIRTHDAY MONTH IS NOVEMBER ",11);
+	 break;
+   case 12:
+	 printf(" YOUR BIRTHDAY MONTH IS DECEMBER ",12);
+	 break;
+  }
+  getch();
+ }
+
