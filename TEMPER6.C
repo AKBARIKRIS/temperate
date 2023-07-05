6c7cdd1edb8124260756ae94d33a0c80481a78d0
AKBARIKRIS <137590850+AKBARIKRIS@users.noreply.github.com>
2023-07-05 19:51:46 +0530

    Add files via upload

---
+++ TEMPER6.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+ void main()
+ {
+  int a=1,i,f=1;
+  clrscr();
+  printf("enter value =");
+  scanf("%i",&i);
+
+  for(i;a<=i;a++)
+  f=f*a;
+  {
+   printf("f=%d \t",f);
+  }
+  getch();
+ }
