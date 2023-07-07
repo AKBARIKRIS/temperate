50f99e0e59ba284ddca733f7becdf2f8ff921707
AKBARIKRIS <137590850+AKBARIKRIS@users.noreply.github.com>
2023-07-07 17:02:10 +0530

    Add files via upload

---
+++ TEMPER7.C
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+ void main()
+ {
+  int i=0,j=1,n,t,a;
+  clrscr();
+  printf(" enter value =");
+  scanf("%d",&n);
+  printf("%d , %d "a,b);
+  for(a=2;)
+  {
+    if(num%a==0)
+    {
+    count++;
+    }
+  }
+  if(count==2)
+  {
+   printf(" \n %d prime number ",num);
+  }
+  else
+  {
+   printf(" \n %d not prime number",num);
+  }
+ getch();
+ }
